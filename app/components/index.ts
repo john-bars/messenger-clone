afb62799560459c04d945b7297b919411fad3f24
John Bars <john.barsq@gmail.com>
2023-09-03 14:25:22 +0800

    organize the import and export of components using the index.ts.

--- app/components/index.ts
+++ app/components/index.ts
@@ -1,4 +1,7 @@
 import Button from "./Button";
 import Input from "./inputs/Input";
+import EmptyState from "./EmptyState";
+import Sidebar from "./sidebar/Sidebar";
+import DesktopSidebar from "./sidebar/DesktopSidebar";
 
-export { Button, Input };
+export { Button, Input, EmptyState, Sidebar, DesktopSidebar };
