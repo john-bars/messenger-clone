dfb1933946c4cf01876eb106462d727b97526e8b
John Bars <john.barsq@gmail.com>
2023-09-30 17:35:31 +0800

    modify the sidebar to display the routes from the /app/hooks. use different layout of sidebar for mobile devices and Desktop

---
+++ app/components/sidebar/MobileFooter.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import useConversation from "@/app/hooks/useConversation";
+import useRoutes from "@/app/hooks/useRoutes";
+import MobileItem from "./MobileItem";
+
+const MobileFooter = () => {
+  const routes = useRoutes();
+  const { isOpen } = useConversation();
+
+  isOpen && null;
+  return (
+    <div className="bottom-0 fixed flex justify-between items-center w-full bg-white z-40 lg:hidden border-t-[1px]">
+      {routes.map((route) => (
+        <MobileItem
+          key={route.href}
+          href={route.href}
+          active={route.active}
+          icon={route.icon}
+          onClick={route.onClick}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default MobileFooter;
