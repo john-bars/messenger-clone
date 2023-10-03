ad08aca5648da12c87f19a6bd8f0da005eb0e787
John Bars <john.barsq@gmail.com>
2023-10-03 15:34:54 +0800

    fix the placeholder image error

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -11,7 +11,7 @@ const Avatar: React.FC<AvatarProps> = ({ user }) => {
     <div className="relative">
       <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
         <Image
-          src={user?.image || "/images.placeholder.jpeg"}
+          src={user?.image || "/images/placeholder.jpeg"}
           alt="Avatar"
           fill
         />
