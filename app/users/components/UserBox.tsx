f1f12b707dc8627ddbb03b3ac942d6875972258b
John Bars <john.barsq@gmail.com>
2023-10-03 15:37:26 +0800

    Show the other users apart from the current user. Displace other user\'s avatar and name.

---
+++ app/users/components/UserBox.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { User } from "@prisma/client";
+import Avatar from "@/app/components/Avatar";
+
+interface UserBoxProps {
+  data: User;
+}
+
+const UserBox: React.FC<UserBoxProps> = ({ data }) => {
+  const router = useRouter();
+  const [isLoding, setIsLoding] = useState(false);
+
+  const handleClick = useCallback(() => {
+    setIsLoding(true);
+
+    axios
+      .post("/api/convetsations", {
+        userId: data.id,
+      })
+      .then((data) => {
+        router.push(`/conversations/${data.data.id}`);
+      })
+      .finally(() => setIsLoding(false));
+  }, [data, router]);
+
+  return (
+    <div
+      onClick={handleClick}
+      className="w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+    >
+      <Avatar user={data} />
+      <div className="min-w-0 flex-1">
+        <div className="focus:outline-none">
+          <div className="flex justify-between items-center mb-1">
+            <p className="text-sm font-medium text-gray-900">{data.name}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserBox;
