7bc612d3b3fed68824cc9d1e73a44c9816482e04
John Bars <john.barsq@gmail.com>
2023-10-04 21:53:17 +0800

    Create a function that will return information of a user appart from the currentUser.

---
+++ app/hooks/useOtherUser.ts
@@ -0,0 +1,27 @@
+import { useSession } from "next-auth/react";
+import { useMemo } from "react";
+import { FullConversationType } from "../types";
+import { User } from "@prisma/client";
+
+const useOtherUser = (
+  conversation:
+    | FullConversationType
+    | {
+        users: User[];
+      }
+) => {
+  const session = useSession();
+
+  const otherUser = useMemo(() => {
+    const currentUserEmail = session?.data?.user?.email; // the email of the current user
+    const otherUser = conversation.users.filter(
+      (user) => user.email !== currentUserEmail //return users that are not equal to the currentUser
+    );
+
+    return otherUser[0]; //get the first otther user
+  }, [session?.data?.user?.email, conversation.users]);
+
+  return otherUser;
+};
+
+export default useOtherUser;
