60ad53dd271c746f183cb9ce18225d169f56f43f
John Bars <john.barsq@gmail.com>
2023-10-05 13:26:06 +0800

    create an action that will handle messages between the currentUser and other users. used to render messages in ascending order; show if the messages are seen and the information of the sender.

---
+++ app/actions/getMessages.ts
@@ -0,0 +1,24 @@
+import prisma from "@/app/libs/prismadb";
+
+const getMessages = async (conversationId: string) => {
+  try {
+    const messages = await prisma.message.findMany({
+      where: {
+        conversationId: conversationId,
+      },
+      include: {
+        sender: true,
+        seen: true,
+      },
+      orderBy: {
+        createdAt: "asc", // to show the latest messages at the top
+      },
+    });
+
+    return messages;
+  } catch (error: any) {
+    return [];
+  }
+};
+
+export default getMessages;
