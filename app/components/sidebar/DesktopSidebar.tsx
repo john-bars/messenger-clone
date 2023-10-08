cc07f6c0451eddcaba04474b4a095a3a86487a07
John Bars <john.barsq@gmail.com>
2023-10-08 13:17:27 +0800

    Render the entire DesktopSidebar inside a Fragment Create a SettingsModal that will show once the user's Avatar part has been clicked.

--- app/components/sidebar/DesktopSidebar.tsx
+++ app/components/sidebar/DesktopSidebar.tsx
@@ -1,12 +1,12 @@
 "use client";
-//CHECKED
 
 import { useState } from "react";
 import useRoutes from "@/app/hooks/useRoutes";
-import DesktopItem from "./DesktopItem";
-
 import { User } from "@prisma/client";
+
 import Avatar from "../Avatar";
+import DesktopItem from "./DesktopItem";
+import SettingsModal from "./SettingsModal";
 
 interface DesktopSidebarProps {
   currentUser: User;
@@ -16,34 +16,41 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
-  // console.log({ currentUser }, "TEST");
+  // console.log("currentUser: ", { currentUser });
 
   return (
-    <div className="lg:flex lg:flex-col justify-between hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto lg:bg-white lg:border-r-[1px] lg:pb-4">
-      <nav className="flex flex-col justify-between mt-4">
-        <ul role="list" className="flex flex-col items-center space-y-1">
-          {routes.map((item) => (
-            <DesktopItem
-              key={item.label}
-              href={item.href}
-              label={item.label}
-              icon={item.icon}
-              active={item.active}
-              onClick={item.onClick}
-            />
-          ))}
-        </ul>
-      </nav>
+    <>
+      <SettingsModal
+        currentUser={currentUser}
+        isOpen={isOpen}
+        onClose={() => setIsOpen(false)}
+      />
+      <div className="lg:flex lg:flex-col justify-between hidden lg:fixed lg:inset-y-0 lg:left-0 lg:z-40 lg:w-20 xl:px-6 lg:overflow-auto lg:bg-white lg:border-r-[1px] lg:pb-4">
+        <nav className="flex flex-col justify-between mt-4">
+          <ul role="list" className="flex flex-col items-center space-y-1">
+            {routes.map((item) => (
+              <DesktopItem
+                key={item.label}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+                active={item.active}
+                onClick={item.onClick}
+              />
+            ))}
+          </ul>
+        </nav>
 
-      <nav className="flex flex-col justify-between items-center mt-4">
-        <div
-          onClick={() => setIsOpen(true)}
-          className="cursor-pointer hover:opacity-75 transition"
-        >
-          <Avatar user={currentUser} />
-        </div>
-      </nav>
-    </div>
+        <nav className="flex flex-col justify-between items-center mt-4">
+          <div
+            onClick={() => setIsOpen(true)}
+            className="cursor-pointer hover:opacity-75 transition"
+          >
+            <Avatar user={currentUser} />
+          </div>
+        </nav>
+      </div>
+    </>
   );
 };
 
