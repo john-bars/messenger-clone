91f76142b4cd29a4159c64e62b0e228b4e79df6f
John Bars <john.barsq@gmail.com>
2023-10-09 13:49:55 +0800

    Create an avatar for the group that will render the user image of the first 3 users within the group chat as the gc's avatar.

---
+++ app/components/AvatarGroup.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { User } from "@prisma/client";
+import Image from "next/image";
+
+interface AvatarGroupProps {
+  users?: User[];
+}
+
+const AvatarGroup: React.FC<AvatarGroupProps> = ({ users = [] }) => {
+  const slicedUsers = users.slice(0, 3); //get the first 3 users
+
+  //   const positionMap = ["top-0 left-3", "bottom-0", "bottom-0 right-0"];
+
+  const positionMap = {
+    0: "top-0 left-3",
+    1: "bottom-0",
+    2: "bottom-0 right-0",
+  };
+
+  return (
+    <div className="relative h-11 w-11">
+      {slicedUsers.map((user, index) => (
+        <div
+          key={user.id}
+          className={`absolute inline-block rounded-full overflow-hidden h-5 w-5 ${
+            positionMap[index as keyof typeof positionMap]
+          }`}
+        >
+          <Image
+            src={user?.image || "/images/placeholder.jpeg"}
+            alt="Avatar"
+            fill
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AvatarGroup;
